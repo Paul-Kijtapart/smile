19c5111e308c3be1d76c4ccddb2fa237ddbbfa12
Kijtapart  Nawattranakul <scc385@gmail.com>
2019-12-30 21:03:41 -0800

    Feat: Added two components.

---
+++ src/happy-new-year-2019.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="happy-new-year">
+    Happy New Year.
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "happy-new-year"
+  }
+</script>
+
+<style scoped>
+
+</style>
